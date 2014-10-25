fe735cffe7ae72e6a7a5dc78c0f77fb69faf523d
congyuandong <congyuandong@gmail.com>
2014-10-25 19:49:52 +0800

    修改wsgi

--- logistics/wsgi.py
+++ logistics/wsgi.py
@@ -19,7 +19,8 @@ sys.path.append(apache_configuration)
 sys.path.append(project)
 sys.path.append(workspace)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logistics.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logistics.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "logistics.settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
