0fc1a0d1252ce1b869d77e8f5650724b155cd049
congyuandong <congyuandong@gmail.com>
2014-10-25 19:17:02 +0800

    修改wsgi文件

--- logistics/wsgi.py
+++ logistics/wsgi.py
@@ -9,12 +9,21 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import sys
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logistics.settings")
+
 
 apache_configuration= os.path.dirname(__file__)
 project = os.path.dirname(apache_configuration)
 workspace = os.path.dirname(project)
+
+sys.path.append(apache_configuration)
+sys.path.append(project)
 sys.path.append(workspace)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logistics.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+if __name__ == '__main__':
+	print workspace,apache_configuration,project
